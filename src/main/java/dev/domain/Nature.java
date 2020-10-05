1ec0ced171723f4db6626794a3f1b5f9e9d1a4a6
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-10-05 12:19:00 +0200

    Créer entité Nature #23

---
+++ src/main/java/dev/domain/Nature.java
@@ -0,0 +1,179 @@
+package dev.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Nature {
+	
+	private String nom;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private boolean missionFacturee;
+	private boolean versementPrime;
+	private BigDecimal tjm;
+	private BigDecimal pourcentagePrime;
+	private BigDecimal plafond;
+	private boolean plafondDepassable;
+	
+	@NotNull
+	@Column(name = "debut_validite")
+	private LocalDate debutValidite;
+	
+	@Column(name = "fin_validite")
+	private LocalDate finValidite;
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the missionFacturee
+	 */
+	public boolean isMissionFacturee() {
+		return missionFacturee;
+	}
+
+	/**
+	 * @param missionFacturee the missionFacturee to set
+	 */
+	public void setMissionFacturee(boolean missionFacturee) {
+		this.missionFacturee = missionFacturee;
+	}
+
+	/**
+	 * @return the versementPrime
+	 */
+	public boolean isVersementPrime() {
+		return versementPrime;
+	}
+
+	/**
+	 * @param versementPrime the versementPrime to set
+	 */
+	public void setVersementPrime(boolean versementPrime) {
+		this.versementPrime = versementPrime;
+	}
+
+	/**
+	 * @return the tjm
+	 */
+	public BigDecimal getTjm() {
+		return tjm;
+	}
+
+	/**
+	 * @param tjm the tjm to set
+	 */
+	public void setTjm(BigDecimal tjm) {
+		this.tjm = tjm;
+	}
+
+	/**
+	 * @return the pourcentagePrime
+	 */
+	public BigDecimal getPourcentagePrime() {
+		return pourcentagePrime;
+	}
+
+	/**
+	 * @param pourcentagePrime the pourcentagePrime to set
+	 */
+	public void setPourcentagePrime(BigDecimal pourcentagePrime) {
+		this.pourcentagePrime = pourcentagePrime;
+	}
+
+	/**
+	 * @return the plafond
+	 */
+	public BigDecimal getPlafond() {
+		return plafond;
+	}
+
+	/**
+	 * @param plafond the plafond to set
+	 */
+	public void setPlafond(BigDecimal plafond) {
+		this.plafond = plafond;
+	}
+
+	/**
+	 * @return the plafondDepassable
+	 */
+	public boolean isPlafondDepassable() {
+		return plafondDepassable;
+	}
+
+	/**
+	 * @param plafondDepassable the plafondDepassable to set
+	 */
+	public void setPlafondDepassable(boolean plafondDepassable) {
+		this.plafondDepassable = plafondDepassable;
+	}
+
+	/**
+	 * @return the debutValidite
+	 */
+	public LocalDate getDebutValidite() {
+		return debutValidite;
+	}
+
+	/**
+	 * @param debutValidite the debutValidite to set
+	 */
+	public void setDebutValidite(LocalDate debutValidite) {
+		this.debutValidite = debutValidite;
+	}
+
+	/**
+	 * @return the finValidite
+	 */
+	public LocalDate getFinValidite() {
+		return finValidite;
+	}
+
+	/**
+	 * @param finValidite the finValidite to set
+	 */
+	public void setFinValidite(LocalDate finValidite) {
+		this.finValidite = finValidite;
+	}
+	
+	
+
+}
