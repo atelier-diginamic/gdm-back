6b884c6162134614d33c570f0424e1c080f6f3c7
Helvin <hjwc86@gmail.com>
2020-10-06 17:23:31 +0200

    list mission #18

--- src/main/java/dev/controller/mission/MissionController.java
+++ src/main/java/dev/controller/mission/MissionController.java
@@ -1,5 +1,29 @@
 package dev.controller.mission;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.domain.Mission;
+import dev.service.MissionService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("missions")
 public class MissionController {
+
+	private MissionService missionService;
+
+	public MissionController(MissionService missionService) {
+		this.missionService = missionService;
+	}
+
+	@GetMapping
+	public List<Mission> listeNotesDeFrais() {
+		return missionService.getList();
+	}
 
 }
