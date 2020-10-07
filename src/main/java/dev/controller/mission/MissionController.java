5666d82f32075af6593d70ce72392ab1cf881eaf
Helvin <hjwc86@gmail.com>
2020-10-07 10:30:52 +0200

    get missions by college #18

--- src/main/java/dev/controller/mission/MissionController.java
+++ src/main/java/dev/controller/mission/MissionController.java
@@ -1,13 +1,16 @@
 package dev.controller.mission;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.domain.Mission;
+import dev.domain.Collegue;
+import dev.repository.CollegueRepo;
 import dev.service.MissionService;
 
 @RestController
@@ -16,14 +19,25 @@ import dev.service.MissionService;
 public class MissionController {
 
 	private MissionService missionService;
+	private CollegueRepo collegueRepository;
 
-	public MissionController(MissionService missionService) {
+	public MissionController(MissionService missionService, CollegueRepo collegueRepository) {
 		this.missionService = missionService;
+		this.collegueRepository = collegueRepository;
+
 	}
 
-	@GetMapping
-	public List<Mission> listeMissions() {
-		return missionService.getList();
+	@GetMapping("{idCollegue}")
+	public ResponseEntity<?> listeMissions(@PathVariable Long idCollegue) {
+
+		Optional<Collegue> collegueOp = collegueRepository.findById(idCollegue);
+
+		if (collegueOp.isPresent()) {
+			return ResponseEntity.ok(collegueOp.get().getMissions());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 
 }
